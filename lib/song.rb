e464272bdf0875c71170a68d925d8c8625dda1d1
Romario Gustave <romagustave10@gmail.com>
2020-01-03 20:43:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,32 @@
-class Song
-
-  attr_accessor :name, :artist
-
+class Song 
+  attr_accessor :artist, :name   
+  @@all = []
+  
   def initialize(name)
-    @name = name
-  end
-
+    @name = name 
+    save 
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  
+  def self.all
+    #Artist.all 
+    @@all 
+  end 
+  
   def self.new_by_filename(file_name)
-    song = file_name.split(" - ")[1]
     artist = file_name.split(" - ")[0]
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
-  end
-
+    song_title = file_name.split(" - ")[1]
+    song = self.new(song_title)
+    song.artist_name = artist
+    song 
+  end 
+  
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
-
-end
+    self.artist.add_song(self) 
+  end 
+end 
